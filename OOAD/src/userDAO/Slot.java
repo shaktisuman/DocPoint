c9a39dc321f5ef12db80edcefdb84dab5c711692
Max <maxchen507>
2018-09-28 21:19:25 -0500

    Slot initial Commit

---
+++ OOAD/src/userDAO/Slot.java
@@ -0,0 +1,73 @@
+package userDAO;
+
+public class Slot {
+	
+	private int slot_ID;
+	private int start_t;
+	private int end_t;
+	private int doc_Id;
+		
+//	Empty Constructor
+	Slot (){
+		this.slot_ID = -1;
+		this.start_t = -1;
+		this.end_t = -1;
+		this.doc_Id = -1;
+	}
+	
+//	Full Constructor
+	Slot (int slot_ID, int start_t, int end_t, int doc_Id)
+	{
+		this.slot_ID = slot_ID;
+		this.start_t = start_t;
+		this.end_t = end_t;
+		this.doc_Id = doc_Id;
+	}
+
+//	Constructor without slot_ID
+	Slot (int start_t, int end_t, int doc_Id)
+	{
+		this.start_t = start_t;
+		this.end_t = end_t;
+		this.doc_Id = doc_Id;
+	}	
+	
+	//Getters and Setters
+	public int getSlot_ID() {
+		return slot_ID;
+	}
+
+	public void setSlot_ID(int slot_ID) {
+		this.slot_ID = slot_ID;
+	}
+
+	public int getStart_t() {
+		return start_t;
+	}
+
+	public void setStart_t(int start_t) {
+		this.start_t = start_t;
+	}
+
+	public int getEnd_t() {
+		return end_t;
+	}
+
+	public void setEnd_t(int end_t) {
+		this.end_t = end_t;
+	}
+
+	public int getDoc_Id() {
+		return doc_Id;
+	}
+
+	public void setDoc_Id(int doc_Id) {
+		this.doc_Id = doc_Id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Slot [slot_ID=" + slot_ID + ", start_t=" + start_t + ", end_t=" + end_t + ", doc_Id=" + doc_Id + "]";
+	}
+
+}
---
+++ OOAD/src/userDAO/SlotDao.java
@@ -0,0 +1,21 @@
+package userDAO;
+
+import userDAO.Slot;
+import java.util.List;
+
+public interface SlotDao {
+	
+	public Slot getSlot(int Slot_Id);
+	
+	public int addSlot(Slot s);
+	public void updateSlot(Slot s);
+	public void deleteSlot(int Slot_Id);
+	
+	public void deleteAllSlotsForDoc(int Doc_Id);
+	
+	public List<Slot> getSlotsForDoc(int Doc_Id);
+	public int getTotalNumSlotsForDoc(int Doc_Id);
+	
+	public List<Slot> getAvailSlotsForDocOnDate(int Doc_Id, String Date);
+	
+}
