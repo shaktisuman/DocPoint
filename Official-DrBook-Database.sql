3bf172f67a52aa34e97c941a2501873f6e9d37e6
Max Chen <maxchen507@gmail.com>
2018-10-06 11:46:17 -0500

    Add files via upload

--- Official-DrBook-Database.sql
+++ Official-DrBook-Database.sql
@@ -71,15 +71,15 @@ INSERT INTO DRBOOK_ADMIN VALUES ('1');
 INSERT INTO DOC VALUES ('1', 'John Smith', 'johnsmith@email.com', 'password', 'Pediatrician', 'I am a doctor that specializes in treating children.');
 INSERT INTO DOC VALUES ('2', 'Sarah Palmer', 'sarahpalmer@email.com', 'password',  'ENT', 'I am a doctor that specializes in ear, nose, throat.');
 
-INSERT INTO SLOT VALUES ('1', '540', '600', '2');
-INSERT INTO SLOT VALUES ('2', '600', '660', '2');
-INSERT INTO SLOT VALUES ('3', '480', '540', '3');
+INSERT INTO SLOT VALUES ('1', '540', '600', '1');
+INSERT INTO SLOT VALUES ('2', '600', '660', '1');
+INSERT INTO SLOT VALUES ('3', '480', '540', '2');
 
-INSERT INTO PATIENT VALUES ('1', 'Linh Truong', 'linh@emailcom', 'password',  'This is my info for Linh', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
-INSERT INTO PATIENT VALUES ('2', 'Max Chen', 'max@email.com', 'password',  'This is my info for Max', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
-INSERT INTO PATIENT VALUES ('3', 'Nancy Xie', 'nancyX@email.com', 'password',  'This is my info for Nancy', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
+INSERT INTO PATIENT VALUES ('1', 'Linh Truong', 'linh@emailcom', 'password', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
+INSERT INTO PATIENT VALUES ('2', 'Max Chen', 'max@email.com', 'password', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
+INSERT INTO PATIENT VALUES ('3', 'Nancy Xie', 'nancyX@email.com', 'password', 'Secret Q', 'Secret A', '11-11-1990', 'dummy address', 'city', 'state', 'med hist', 'gender');
 
-INSERT INTO APPT VALUES ('1', '2018-09-21', '1', '1', '2');
-INSERT INTO APPT VALUES ('2', '2018-09-21', '2', '2', '2');
-INSERT INTO APPT VALUES ('3', '2018-09-22', '1', '3', '2');
-INSERT INTO APPT VALUES ('4', '2018-09-22', '3', '2', '3');
+INSERT INTO APPT VALUES ('1', '2018-09-21', '1', '1', '1');
+INSERT INTO APPT VALUES ('2', '2018-09-21', '2', '2', '1');
+INSERT INTO APPT VALUES ('3', '2018-09-22', '1', '3', '1');
+INSERT INTO APPT VALUES ('4', '2018-09-22', '3', '2', '2');
