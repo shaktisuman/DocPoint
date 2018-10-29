a33ab894e9c353be550b142426dd0a5ff24ee90b
shaktisuman <32207671+shaktisuman@users.noreply.github.com>
2018-10-28 22:18:58 -0500

    Data Processing updates

--- OOAD/src/bookDAO/BookController.java
+++ OOAD/src/bookDAO/BookController.java
@@ -31,11 +31,9 @@ public class BookController extends HttpServlet {
 			
 			List<Doctor> docList = doctorDao.getAllDoctors();
 			List<String> docListFormat = new ArrayList<String>();
-			List<String> docNames = new ArrayList<String>();
 			
 			for (Doctor doc : docList) {
 				docListFormat.add(doc.getID() + "_" + doc.getName());
-				docNames.add(doc.getName());
 			}
 			
 			//Print out doctor format
@@ -43,21 +41,24 @@ public class BookController extends HttpServlet {
 				//System.out.println(docString);
 			}
 			
-			//Sending list of doctors to populate dropdown in UI
 			String submitType = request.getParameter("submit");
+			
+			//Sending list of doctors to populate dropdown in UI
 			if(submitType.equals("fetchDList"))
 			{
 				System.out.println("Sending list of doctors to populate dropdown in UI");
-				request.setAttribute("names", docNames);
+				request.setAttribute("names", docListFormat);
 				request.setAttribute("firstLoad", "done");
 				request.getRequestDispatcher("bookAppointment.jsp").include(request, response);
 			}
 
+			//Receiving selected Doctor from dropdown and send back the dates for which the doctor is busy
 			if(submitType.equals("dlist"))
 			{
 				String[] SelectedDoc = request.getParameterValues("docList");
 				System.out.println("Printing selected doctor below");
-				System.out.println(SelectedDoc[0]);
+				System.out.println(SelectedDoc[0].split("_")[1]);
+				System.out.println(SelectedDoc[0].split("_")[0]);
 				request.getRequestDispatcher("bookAppointment.jsp").include(request, response);
 			}
 			
@@ -117,8 +118,8 @@ public class BookController extends HttpServlet {
 			
 		}
 		catch(Exception e){
-			System.out.println("DB Error!");
-			System.out.println(e);
+			//System.out.println(e);
+			System.out.println("");
 		}
 		
 	}
