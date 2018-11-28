fd39aaa00ce283b1f5e20fcb0ba196b98a1d5043
Max <maxchen507>
2018-11-27 20:18:10 -0600

    u2

--- OOAD/src/userDAO/PatientDisplayAppt.java
+++ OOAD/src/userDAO/PatientDisplayAppt.java
@@ -1,14 +1,24 @@
 package userDAO;
 
 public class PatientDisplayAppt {
+	private String docName;
 	private String date;
 	private String start_t;
 	private String end_t;
 
-	public PatientDisplayAppt(String date, String start_t, String end_t){
+	public PatientDisplayAppt(String docName, String date, String start_t, String end_t){
+		this.docName = docName;
 		this.date = date;
 		this.date = start_t;
 		this.date = end_t;
+	}
+	
+	public String getDocName() {
+		return docName;
+	}
+
+	public void setDocName(String docName) {
+		this.docName = docName;
 	}
 	
 	public String getDate() {
